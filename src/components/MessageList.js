883222314419aa8b036f906a9a0f50284f72909c
郷大助 <daisukegoh@goudaisukenoMacBook-Pro.local>
2019-02-24 10:52:33 +0900

    completed assignment: Add Messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,7 +6,8 @@ class MessageList extends Component {
         this.messagesRef = this.props.firebase.database().ref('Messages');
         this.state = {
             messages: [],
-            availableroom: []
+            availableroom: [],
+            newMessage: ""
         };
     }
 
@@ -18,14 +19,34 @@ componentDidMount() {
         });
       }
 
+handleChange(event) {
+        this.setState({newMessage: event.target.value});    
+    }
+
+handleSubmit(event) {
+        this.messagesRef.push({
+            content: this.state.newMessage,
+            username: this.props.user.displayName,
+            roomId: this.props.activeroomkey
+        });
+        event.preventDefault();
+    }
+
 render() {
     return (
         <div>
         <h2>{this.props.activeroom}</h2>
         {
         this.state.messages.filter( message => message.roomId == this.props.activeroomkey).map( (messages) => 
-        <p>{messages.content}</p>  )            
+        <div>
+        <h4>{messages.username}</h4>
+        <p>{messages.content}</p>
+        </div> )            
         }
+        <form onSubmit={ (event) => this.handleSubmit(event) }> 
+                <input type="text" value={this.state.newMessage} onChange={ (event) => this.handleChange(event)} />
+                <input type="submit" value="Submit" />
+                </form>
         </div>
         );
     }
